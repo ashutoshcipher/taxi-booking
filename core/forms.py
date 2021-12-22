f67399012b01d580cf9cb13219098c41435cf944
Ashutosh Gupta <ashutosh.gupta@st.niituniversity.in>
2021-12-22 17:04:45 +0530

    update 1

--- core/forms.py
+++ core/forms.py
@@ -7,3 +7,6 @@ class userinput(forms.Form):
     def __init__(self, *args, **kwargs):
         super(userinput, self).__init__(*args, **kwargs)
         self.fields['q'].widget = forms.TextInput(attrs={'placeholder': 'Set Id', 'style' :'margin-left: 50px;margin-right: 10px;width:50%'})
+        #change one
+        #change two
+        #change 4
